0536b27d2d7847101a732acce63e9e07a2eeba0c
Astro <rgrxmahabub.official@gmail.com>
2023-05-18 16:42:32 +0600

    firebase and authprovider added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Lottie from 'react-lottie';
 
 const Login = () => {
     return (
         <div>
             This is log in page
+            <Lottie></Lottie>
         </div>
     );
 };
