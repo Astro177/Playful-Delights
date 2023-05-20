fac8f61cc11406f3dc4846c6da3725dc3b34a0b1
Astro <rgrxmahabub.official@gmail.com>
2023-05-20 22:39:26 +0600

    blogs added

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -19,7 +19,12 @@ const Blogs = () => {
           work and where should we store them on the client-side?
         </div>
         <div className="collapse-content">
-          <p className="font-semibold">Ans:</p>
+          <p className="font-semibold">
+            Ans: An access token and a refresh token are a commonly used systems
+            that authenticates to grant and manage access to protected data`s.{" "}
+            <br /> They authenticates users, gives access to their data handles
+            expiration, refreshes the token and repeat the process.
+          </p>
         </div>
       </div>
       <div
@@ -30,7 +35,11 @@ const Blogs = () => {
           Question #2: What is the comparison between SQL and NoSQL databases?
         </div>
         <div className="collapse-content">
-          <p className="font-semibold">Ans:</p>
+          <p className="font-semibold">
+            Ans: The main comparison between SQL and NoSQL database is that, SQL
+            database follows a structured, tabular data model, where NoSQL
+            database offers a flexible, schema-less data model.
+          </p>
         </div>
       </div>
       <div
@@ -41,7 +50,15 @@ const Blogs = () => {
           Question #3: What is express js? What is Nest JS?
         </div>
         <div className="collapse-content">
-          <p className="font-semibold">Ans:</p>
+          <p className="font-semibold">
+            Ans: Express Js is a minimalist and flexible web application
+            framework for Node.js. It provides a simple and straightforward API
+            for building web applications and API`s. <br />
+            Nest js is a progressive, opinionated framework for building
+            efficient and scalable server-side applications with Node Js. It is
+            built on top of Express Js but provides a higher-level abstraction
+            and additional features.
+          </p>
         </div>
       </div>
       <div
@@ -52,7 +69,18 @@ const Blogs = () => {
           Question #4: What is MongoDB aggregate and how does it work?
         </div>
         <div className="collapse-content">
-          <p className="font-semibold">Ans:</p>
+          <p className="font-semibold">
+            Ans: In MongoDB aggregate is a operation that is used to perform
+            advanced data processing and analysis on the documents within a
+            collection. It allows you to run complex queries and perform various
+            transformations, aggregations, and computations on the data. <br />
+            The aggregate operation in MongoDB works by defining a pipeline of
+            stages that are executed sequentially on the documents. Each stage
+            in the pipeline performs a specific operation on the data and passes
+            the modified dataset to the next stage. The stages can include
+            various operations such as filtering, grouping, projecting, sorting,
+            joining, and more.
+          </p>
         </div>
       </div>
     </div>
--- src/secondsection/SecondSection.jsx
+++ src/secondsection/SecondSection.jsx
@@ -8,19 +8,31 @@ const SecondSection = () => {
       </p>
       <hr className="border-2 w-12 mx-auto mb-24" />
       <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-24 mb-24">
-        <div>
-          <p className="w-32 h-32 bg-purple-800 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">1</p>
-          <p className="text-lg font-semibold text-pink-500 mb-2">Variety of toys</p>
+        <div data-aos="fade-right" data-aos-duration="2000">
+          <p className="w-32 h-32 bg-purple-800 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">
+            1
+          </p>
+          <p className="text-lg font-semibold text-pink-500 mb-2">
+            Variety of toys
+          </p>
           <p>You can choose from our vast collection of toys</p>
         </div>
-        <div>
-          <p className="w-32 h-32 bg-green-600 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">2</p>
-          <p className="text-lg font-semibold text-pink-500 mb-2">Customization</p>
+        <div data-aos="fade-up" data-aos-duration="2000">
+          <p className="w-32 h-32 bg-green-600 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">
+            2
+          </p>
+          <p className="text-lg font-semibold text-pink-500 mb-2">
+            Customization
+          </p>
           <p>You can customize your own toy just the way you like it</p>
         </div>
-        <div>
-          <p className="w-32 h-32 bg-yellow-600 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">3</p>
-          <p className="text-lg font-semibold text-pink-500 mb-2">Fast Delivery</p>
+        <div data-aos="fade-left" data-aos-duration="2000">
+          <p className="w-32 h-32 bg-yellow-600 rounded-full text-center text-5xl font-bold text-slate-300 mb-6">
+            3
+          </p>
+          <p className="text-lg font-semibold text-pink-500 mb-2">
+            Fast Delivery
+          </p>
           <p>We can deliver any toys you order within 24 hours</p>
         </div>
       </div>
