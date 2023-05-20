2cc2c2ff4c78ee05d63825bd97d280b7f5e770ee
Astro <rgrxmahabub.official@gmail.com>
2023-05-20 10:48:58 +0600

    first unique section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Banner from "./Banner";
 import Gallery from "./Gallery";
 import SubTabs from "./SubTabs";
+import FirstSection from "../firstsection/FirstSection";
 
 const Home = () => {
   return (
     <>
-      <Banner/>
-      <Gallery/>
-      <SubTabs/>
+      <Banner />
+      <Gallery />
+      <SubTabs />
+      <FirstSection />
     </>
   );
 };
--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -10,16 +10,16 @@ const ToyDetails = () => {
     <div className="my-container">
       <div className="card lg:card-side bg-base-100 shadow-xl p-6">
         <figure>
-          <img src={toy.toyPictureUrl} alt="toy" />
+          <img className="rounded-xl lg:w-full" src={toy.toyPictureUrl} alt="toy" />
         </figure>
         <div className="card-body text-center">
           <h2 className="text-5xl text-color font-bold mb-6">{toy.toyName}</h2>
           <h2 className="text-slate-400 mb-8">{toy.description}</h2>
-          <p className="text-lg font-semi">Seller Name: {toy.sellerName}</p>
-          <p className="text-lg font-semi">Seller Email: {toy.sellerEmail}</p>
-          <p className="text-lg font-semi">Category: {toy.toyCategory}</p>
-          <p className="text-lg font-semi">Price: {toy.price} $</p>
-          <p className="text-lg font-semi">
+          <p className="text-lg font-semibold">Seller Name: {toy.sellerName}</p>
+          <p className="text-lg font-semibold">Seller Email: {toy.sellerEmail}</p>
+          <p className="text-lg font-semibold">Category: {toy.toyCategory}</p>
+          <p className="text-lg font-semibold">Price: {toy.price} $</p>
+          <p className="text-lg font-semibold">
             Available Quantity: {toy.availableQuantity} pcs
           </p>
         </div>
